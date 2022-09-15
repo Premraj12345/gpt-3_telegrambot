97b9ba8eedd0d0695bd9c6f5ca9ccf25e1933793
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-09-15 16:36:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters 
 import openai
 
-api_key = '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'
+api_key = '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'
 openai.api_key = api_key
 
 updater = Updater('5383451954:AAGx7pk-duETmTo-nH7NyEyXaLjqwkA0VoQ', use_context=True) 
